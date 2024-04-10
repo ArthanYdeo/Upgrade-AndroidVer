843d6080cf58ae37d8b2304bd145a6f48436a987
TopGeee <120080169+TopGeee@users.noreply.github.com>
2024-04-11 02:43:06 +0800

    update
    chapter 1 outro comic style dialogue updated

--- Assets/Scripts/LevelPoint/NextLevelUnlocked.cs
+++ Assets/Scripts/LevelPoint/NextLevelUnlocked.cs
@@ -7,15 +7,17 @@ public class NextLevelUnlocked : MonoBehaviour
 {
 
     public GameManagerScript gameManager;
+    public string sceneName;
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
             // Player collided with an obstacle, game over
             UnlockNewLevel();
-            gameManager.Dialogue();
+            //gameManager.Dialogue();
+            SceneManager.LoadScene(sceneName);
             gameManager.gameWin();
-            Time.timeScale = 0; // Pause the game
+            //Time.timeScale = 0; // Pause the game
         }
 
     }
